c76bf514e336a6faa7aa37f9e77ee420865233e2
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2024-03-07 09:30:55 +0530

    Fixed designing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import Details from "./Details";
 
 const Contact = () => {
   return (
-    <div className="container mx-auto py-8 px-4 bg-yellow h-screen">
+    <div className="container mx-auto py-8 px-4 bg-yellow h-full md:h-screen">
       <h1 className="text-3xl font-bold text-center mb-8">Contact Me</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:mx-12 mx-10">
